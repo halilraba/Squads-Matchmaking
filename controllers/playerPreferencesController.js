f7747ad5623d4ea61709c01366af8c63af55aa4d
Dickson Diku <dixsonlukis@gmail.com>
2021-03-21 08:25:08 -0400

    added getPreferenceById endpoint

--- controllers/playerPreferencesController.js
+++ controllers/playerPreferencesController.js
@@ -1,60 +1,52 @@
 const mongoose = require('mongoose');
-<<<<<<< HEAD
 const PlayerPreferences = require(__dirname + './../models/preferences-model');
-=======
-const Preference = require('../models/preferences-model');
->>>>>>> fd4dd2f3fb7e5939eead7b1217950894ace7d0d2
 
 exports.createPlayerPreferences = (req, res) => {
         
-    const playerPreferences = new Preference({
-        email: req.session.email,
-        duos: req.body.duos,
-        trios: req.body.trios,
-        squads: req.body.squads,
-        casual: req.body.casual,
-        ranked: req.body.ranked,
-        competitions: req.body.competitions,
-        exhibitions: req.body.exhibitions,
-        funScale: req.body.funScale,
-        riskScale:req.body.riskScale
+    const playerPreferences = new PlayerPreferences({
+        _id: new mongoose.Types.ObjectId(),
+        numberOfPlayers: req.body.numberOfPlayers,
+        rankingMode:req.body.rankingMode,
+        playMode: req.body.playMode,
+        competitionMode: req.body.competitionMode,
+        riskMode: req.body.riskMode
+
     });
     
     playerPreferences
         .save()
         .then(result => {
+            console.log(result);
+            //res.redirect('/preferences')
+            res.status(201).json({
+                message: 'Player preferences successfully added',
+                createdPreferences:{
+                    numberOfPlayers: result.numberOfPlayers,
+                    rankingMode:result.rankingMode,
+                    playMode: result.playMode,
+                    competitionMode: result.competitionMode,
+                    riskMode: result.riskMode,
+                    _id: result._id,
 
-            res.redirect('/profile')
-            // res.status(201).json({
-            //     message: 'Player preferences successfully added',
-            //     createdPreferences:{
-            //         numberOfPlayers: result.numberOfPlayers,
-            //         rankingMode:result.rankingMode,
-            //         playMode: result.playMode,
-            //         competitionMode: result.competitionMode,
-            //         riskMode: result.riskMode,
-            //         _id: result._id,
-
-            //         request:{
-            //             type:'POST',
-            //             url:'/preferences' + result._id
-            //         }
-            //     }
+                    request:{
+                        type:'POST',
+                        url:'http://localhost:3000/preferences' + result._id
+                    }
+                }
                
-            // });
+            });
         })
         .catch(err => {
             console.log(err);
-            res.status(500).json({error: err});
-        });
+        res.status(500).json({error: err});
+    });
 }
 
-<<<<<<< HEAD
 
 exports.getPlayersPreferences = (res, req) => {
     
     PlayerPreferences.find()
-    .select(' numberOfPlayers rankingMode playMode competitionMode riskMode_id')
+    .select(' numberOfPlayers rankingMode playMode competitionMode riskMode _id')
     .exec()
     .then( docs => {
         const response = {
@@ -85,15 +77,28 @@ exports.getPlayersPreferences = (res, req) => {
     });
 }
 
-=======
-exports.getPlayerPreferences = (req, res) => {
-
-    if (req.isAuthenticated()){
-        // res.sendFile(__dirname + '/../views/preferences-form.html');
-        res.sendFile('preferences-form.html', {'root': './views'});
-    } else {
-        res.redirect("/signin");
-    }
-
+exports.getPreferenceById =  (req, res, next) => {
+    const id = req.params.preferenceId;
+    PlayerPreferences.findOne(id)
+    .select('numberOfPlayers rankingMode playMode competitionMode riskMode _id')
+    .exec()
+    .then(doc => {
+        console.log('From database' + doc);
+        if (doc){
+            res.status(200).json({
+                preference: doc,
+                request:{
+                    type:'GET',
+                   // url: 'http://localhost/preferences/getPreferenceById' 
+                }
+            });
+        }
+        else{
+            res.status(404).json({message: 'No valid entry found for provided ID'});
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({error: err});
+    });
 }
->>>>>>> fd4dd2f3fb7e5939eead7b1217950894ace7d0d2
--- routes/playerPreferences.route.js
+++ routes/playerPreferences.route.js
@@ -4,17 +4,15 @@ const router = express.Router();
 //const checkAuth
 const playerPreferencesController = require('../controllers/playerPreferencesController');
 
-<<<<<<< HEAD:routes/playerPeferences.route.js
+
 //Handling incoming GET requests
 router.get('/getAllPreferences', playerPreferencesController.getPlayersPreferences);
-=======
-// Handling incoming GET requests
-router.get('/', playerPreferencesController.getPlayerPreferences);
->>>>>>> fd4dd2f3fb7e5939eead7b1217950894ace7d0d2:routes/playerPreferences.route.js
 
 /**
  * Handling POST requests
  */
  router.post('/', playerPreferencesController.createPlayerPreferences);
+
+ router.get('/: preferenceId', playerPreferencesController.getPreferenceById);
 
  module.exports = router;
