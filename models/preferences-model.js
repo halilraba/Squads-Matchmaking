63b215722c6cabe73c6030197f28494e8953abc5
Dickson Diku <dixsonlukis@gmail.com>
2021-03-21 01:31:25 -0400

    updated

--- models/preferences-model.js
+++ models/preferences-model.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 
-<<<<<<< HEAD
 const preferenceSchema = mongoose.Schema({
 
     _id: mongoose.Schema.Types.ObjectId,
@@ -9,35 +8,20 @@ const preferenceSchema = mongoose.Schema({
     playMode:{type: [String], required: [true, 'required' ]},
     competitionMode: {type: [String], required:[ true, 'competion mode required']},
     riskMode:{ type: [String], required: [true,'required']}
-=======
-const preferenceSchema = new mongoose.Schema({
-    email: String,
-    duos: String,
-    trios: String,
-    squads: String,
-    casual: String,
-    ranked: String,
-    competitions: String,
-    exhibitions: String,
-    funScale: Number,
-    riskScale:Number
->>>>>>> fd4dd2f3fb7e5939eead7b1217950894ace7d0d2
 });
 
-// const preferenceSchema = mongoose.Schema({
+module.exports = mongoose.model('PlayerPreferences',preferenceSchema);
 
-//     _id: mongoose.Schema.Types.ObjectId,
-//     numberOfPlayers:{type: String },
-//     rankingMode:{type: String},
-//     playMode:{type: String, },
-//     competitionMode: {type: String},
-//     riskMode:{ type: String}
-// });
+// const mongoose = require("mongoose");
+
+// const preferenceSchema = {
+//     squadsName: String,
+//     preference1: Number
+// };
+
+// const Preference = new mongoose.model("Preference", preferenceSchema);
 
 // module.exports = Preference;
-// module.exports = mongoose.model('PlayerPreferences',preferenceSchema);
 
-const Preference = new mongoose.model("Preference", preferenceSchema);
 
-module.exports = Preference;
-
+   
