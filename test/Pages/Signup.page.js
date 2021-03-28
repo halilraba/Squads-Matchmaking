0eb223aaeaf6f80f6862643dcad109e31b5200ba
Srikanth Ramakrishna <ramakrsr@amazon.com>
2021-03-27 23:28:06 -0700

    adding singup ( adding an new user) to  functional test

--- test/Pages/Signup.page.js
+++ test/Pages/Signup.page.js
@@ -2,11 +2,17 @@ var SignupPage = function(){
 
     this.firstNameHeader=$('body > div > form > label:nth-child(1) > b');
     this.firstNameInputBox=$('body > div > form > input[type=text]:nth-child(2) ');
+    this.lastNameHeader=$('body > div > form > label:nth-child(3) > b');
+    this.lastNameInputBox=$('body > div > form > input[type=text]:nth-child(4) ');
     this.emailHeader=$('body > div > form > label:nth-child(5) > b');
     this.emailInputBox = $('body > div > form > input[type=email]:nth-child(6)');
+     this.birthdate=$('body > div > form > label:nth-child(7) > b');
+     this.birthdateInputBox = $('body > div > form > input[type=date]:nth-child(8)');
      this.squadsmatchmakingusername=$('body > div > form > label:nth-child(9) > b');
      this.squadsmatchmakingusernameInput = $('body > div > form > input[type=text]:nth-child(10)');
- 
+      this.passwordHeader=$('body > div > form > label:nth-child(15) > b');
+      this.passwordInput = $('body > div > form > input[type=text]:nth-child(16)');
+      this.submitButton=$('body > div > form > button');
 
 
 };
--- test/Tests/signup.spec.js
+++ test/Tests/signup.spec.js
@@ -29,15 +29,32 @@ describe('signup',()=>{
              expect(SignupPage.squadsmatchmakingusername.getText()).toEqual("Squads Matchmaking Username");
              expect(SignupPage.squadsmatchmakingusernameInput.getAttribute("required")).toEqual('true');
           });
-//
-//     it('should have password input box and should be required', () => {
-//        expect(SignupPage.passwordHeader.getText()).toEqual("Password");
-//        expect(SignupPage.passwordInputBox.getAttribute("required")).toEqual('true');
-//     });
-//
-//     it('should have signup link to connect signup page', () => {
-//        expect(SignupPage.signupLink.getAttribute("href")).toEqual("https://squads-test.herokuapp.com/signup");
-//     });
+
+
+
+});
+
+describe('Signup Functionalities',()=>{
+
+    var EC = protractor.ExpectedConditions;
+    beforeEach(()=>{
+        browser.waitForAngularEnabled(false);
+        browser.get('https://squads-test.herokuapp.com/signup');
+
+    });
+
+
+
+     it('should   allow user to signup  with valid values', () => {
+        SignupPage.firstNameInputBox.sendKeys("Test_user"+ Math.floor(Math.random() * 20)+"@gmail.com");
+        SignupPage.lastNameInputBox.sendKeys("lastname");
+        SignupPage.emailInputBox.sendKeys("Test_user"+ Math.floor(Math.random() * 20)+"@gmail.com");
+        SignupPage.birthdateInputBox.sendKeys("11/11/1999");
+        SignupPage.squadsmatchmakingusernameInput.sendKeys("UserName"+Math.floor(Math.random() * 20));
+        SignupPage.passwordInput.sendKeys("sdfsdf2342");
+        SignupPage.submitButton.click();
+        expect(browser.getTitle()).toEqual("Preferences Form");
+     });
 
 
 });
