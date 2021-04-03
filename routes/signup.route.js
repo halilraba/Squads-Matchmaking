bcb8519430c8523f002fa06638cf788f431874b2
Brad Griffith <brad.s.griffith@gmail.com>
2021-04-03 10:51:01 -0400

    add error messages to registration page

--- routes/signup.route.js
+++ routes/signup.route.js
@@ -4,7 +4,12 @@ const router = express.Router();
 const signupController = require('../controllers/signupController');
  
 router.get("/", (req, res) => {
-    res.sendFile("new-registration-form.html", {root: "./views"});
+    // res.sendFile("new-registration-form.html", {root: "./views"});
+    res.render("new-registration-form", {
+        squadsErr: "",
+        apexErr: "",
+        fortniteErr: "",
+        emailErr: ""});
 });
 
 router.post('/', signupController.register_new_user);
